821418565e592adca84f027c5f9050f6317e8801
Ivan <fakeperuvian@gmail.com>
2021-10-28 15:16:03 +0300

    dobavil style

--- calc/script.js
+++ calc/script.js
@@ -2,7 +2,8 @@
 document.querySelector('.btn').addEventListener('click', calc1);
 document.querySelector('.btn2').addEventListener('click', calc2);
 document.querySelector('.btn3').addEventListener('click', calc3);
-
+document.querySelector('.btn4').addEventListener('click', calc4);
+document.querySelector('.btn5').addEventListener('click', calc5);
 function calc1() {
     let a = +document.querySelector('.input1').value;
     let b = +document.querySelector('.input2').value;
@@ -63,6 +64,47 @@ function calc3() {
     }
     else if (c=='/'){
         document.querySelector('.otvet').innerHTML="Ответ " + (a/b);
+    }
+    else console.log('poputal bratok');
+}
+function calc4() {
+    let a = +document.querySelector('.input1').value;
+    let b = +document.querySelector('.input2').value;
+    let c = '*';
+    if ( isNaN(a)==true || isNaN(b)==true ) {
+     console.log('Калькулятор для чисел! Оставьте свои грязные шуточки!');
+     document.querySelector('.otvet').innerHTML = 'Калькулятор для чисел!';
+     document.querySelector('.input1').value='';
+     document.querySelector('.input2').value='';
+    return false;
+    }
+    if ( a=='' || b=='' || c=='' ){
+        document.querySelector('.otvet').innerHTML ='error!За пустые поля посажу';
+        return false;
+    }
+    else if (c=='*'){
+        document.querySelector('.otvet').innerHTML="Ответ " + (a*b);
+    }
+    else console.log('poputal bratok');
+}
+
+function calc5() {
+    let a = +document.querySelector('.input1').value;
+    let b = +document.querySelector('.input2').value;
+    let c = '%';
+    if ( isNaN(a)==true || isNaN(b)==true ) {
+     console.log('Калькулятор для чисел! Оставьте свои грязные шуточки!');
+     document.querySelector('.otvet').innerHTML = 'Калькулятор для чисел!';
+     document.querySelector('.input1').value='';
+     document.querySelector('.input2').value='';
+    return false;
+    }
+    if ( a=='' || b=='' || c=='' ){
+        document.querySelector('.otvet').innerHTML ='error!За пустые поля посажу';
+        return false;
+    }
+    else if (c=='%'){
+        document.querySelector('.otvet').innerHTML='Ответ - ' + a%b ;
     }
     else console.log('poputal bratok');
 }
