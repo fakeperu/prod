6e0237e7d3197571d375e8a7fe8f8e598ec515c0
Ivan <fakeperuvian@gmail.com>
2021-11-01 15:29:23 +0300

    add **

--- node_calc.js
+++ node_calc.js
@@ -2,30 +2,33 @@ function calc(num1, num2, action) {
     if ( typeof num1 !== 'number' || typeof num2 !== 'number'|| isNaN(num1)==true || isNaN(num2)==true ) {
         return('Калькулятор для чисел! Оставьте свои грязные шуточки!');
         }
-    if ( num1==undefined || num2 ==undefined || action=='' ){
+    if ( !num1 || !num2 || action=='' ){
         return('error!За пустые поля накажу');
     }
-    if (action=='+')   {
+    if (action==='+')   {
         return (num1 + num2);
     }
-    if (action=='-')   {
+    if (action==='-')   {
         return(num1 - num2);
     }
-    if (action=='*')   {
+    if (action==='*')   {
         return(num1 * num2);
     }
-    if (action=='/')   {
-        if(num2 == 0){
+    if (action==='/')   {
+        if(num2 === 0){
             return ('На ноль делить неправославно!');
             }
         return(num1 / num2);
     }
-    if (action=='%')   {
-        if(num2 == 0){
+    if (action==='%')   {
+        if(num2 === 0){
             return('На ноль делить неправославно!');
             }
         return(num1 % num2);
     }
+    if (action==='**'){
+        return(num1 ** num2);
+    }
     else return ('Ошибка! Калькулятор не знает что делать и очень грустит')
 }
-console.log(calc(8, 56 , ' '));
+console.log(calc(8, 2, '**'));
