6e4e5fb5c1729b7f95da4f90cd797dd92e4e48b5
Ivan <fakeperuvian@gmail.com>
2021-11-01 15:35:07 +0300

    style update

--- node_calc.js
+++ node_calc.js
@@ -1,32 +1,32 @@
 function calc(num1, num2, action) {
-    if ( typeof num1 !== 'number' || typeof num2 !=='number'|| isNaN(num1)===true || isNaN(num2)===true ) {
+    if ( typeof num1 !== 'number' || typeof num2 !=='number'|| isNaN(num1) === true || isNaN(num2) === true ) {
         return('Калькулятор для чисел! Оставьте свои грязные шуточки!');
         }
-    if ( num1==undefined || num2 ==undefined || action==='' ){
+    if ( num1 === undefined || num2 === undefined || action=== ''){
         return('error!За пустые поля накажу');
     }
-    if (action==='+')   {
+    if (action === '+')   {
         return (num1 + num2);
     }
-    if (action==='-')   {
+    if (action === '-')   {
         return(num1 - num2);
     }
-    if (action==='*')   {
+    if (action === '*')   {
         return(num1 * num2);
     }
-    if (action==='/')   {
+    if (action === '/')   {
         if(num2 === 0){
             return ('На ноль делить неправославно!');
             }
         return(num1 / num2);
     }
-    if (action==='%')   {
+    if (action === '%')   {
         if(num2 === 0){
             return('На ноль делить неправославно!');
             }
         return(num1 % num2);
     }
-    if (action==='**'){
+    if (action === '**'){
         return(num1 ** num2);
     }
     else return ('Ошибка! Калькулятор не знает что делать и очень грустит')
