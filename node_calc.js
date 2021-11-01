0251fd10ec6dc54cc1fabf75c7fe62b4efe696e6
Ivan <fakeperuvian@gmail.com>
2021-11-01 15:33:40 +0300

    technical update

--- node_calc.js
+++ node_calc.js
@@ -2,7 +2,7 @@ function calc(num1, num2, action) {
     if ( typeof num1 !== 'number' || typeof num2 !=='number'|| isNaN(num1)===true || isNaN(num2)===true ) {
         return('Калькулятор для чисел! Оставьте свои грязные шуточки!');
         }
-    if ( !num1 || !num2 || action==='' ){
+    if ( num1==undefined || num2 ==undefined || action==='' ){
         return('error!За пустые поля накажу');
     }
     if (action==='+')   {
